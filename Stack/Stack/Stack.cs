8670bca1fd8d3581a02a05438c5db2da87446cc6
Dmitriy Smirnov <dreps350@gmail.com>
2020-05-03 17:51:47 +0300

    stack added

---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Stack<T>
+    {
+        public LinkedList<T> innerList;
+        public Stack()
+        {
+            // инициализация внутреннего хранилища стека
+            innerList = new LinkedList<T>();
+        }
+
+        public int Size()
+        {
+            // размер текущего стека		  
+            return innerList.Count;
+        }
+
+        public T Pop()
+        {
+            T value = Peek();
+            innerList.RemoveLast();
+            return value;
+        }
+
+        public void Push(T val)
+        {
+            // ваш код
+            innerList.AddLast(new LinkedListNode<T>(val));
+        }
+
+        public T Peek()
+        {
+            // ваш код
+            if (innerList.Count == 0)
+            {
+                return default(T); // null, если стек пустой
+            }
+            return (T)innerList.Last.Value;
+        }
+    }
+
+}
