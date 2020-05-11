c82680571caa2d6c7fe10259d8b0554a1e77208e
Dmitriy Smirnov <dreps350@gmail.com>
2020-05-11 18:41:32 +0300

    Queue added

---
+++ Queue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace AlgorithmsDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<int>();
+            foreach (var item in new int[] { 1, 2, 3, 4, 5 }) {
+                queue.Enqueue(item);
+            }
+            for (var counter = 0; queue.Size() > 0; counter++)
+            {
+                var item = queue.Dequeue();
+                Console.WriteLine($"Handling item #{item}");
+                Thread.Sleep(1000);
+                if (counter % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+        }
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class Queue<T>
+    {
+        LinkedList<T> storage;
+        public Queue() => storage = new LinkedList<T>();
+
+        public void Enqueue(T item) => storage.AddLast(item);
+
+        public T Dequeue()
+        {
+            if (storage.Count == 0)
+            {
+                return default(T);
+            }
+            var item = storage.First.Value;
+            storage.RemoveFirst();
+            return item;
+        }
+
+        public int Size() => storage.Count;
+    }
+}
